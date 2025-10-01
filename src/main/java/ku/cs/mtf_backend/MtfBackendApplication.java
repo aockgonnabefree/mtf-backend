1f108bad08a0b7ee6a525513464003f177373969
aock Chaiyawat <aock2547@gmail.com>
2025-10-02 00:29:19 +0700

    initial commit

---
+++ src/main/java/ku/cs/mtf_backend/MtfBackendApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.mtf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cs/mtf_backend/MtfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.mtf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
