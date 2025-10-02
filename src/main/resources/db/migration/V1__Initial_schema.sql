b7b8d3d1358dc82246368712695ecc6ffbe6f6ef
aock Chaiyawat <aock2547@gmail.com>
2025-10-02 17:41:47 +0700

    fixed: change v1 initial schema

--- src/main/resources/db/migration/V1__Initial_schema.sql
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -1,34 +1,19 @@
-DROP TABLE IF EXISTS employer CASCADE;
-
-DROP TABLE IF EXISTS employee CASCADE;
-
-DROP TABLE IF EXISTS address CASCADE;
-
-DROP TABLE IF EXISTS AGENT CASCADE;
-
-DROP TABLE IF EXISTS EMPLOYMENT_CONTRACT CASCADE;
-
-DROP TABLE IF EXISTS WP_46 CASCADE;
-
-DROP TABLE IF EXISTS WORK CASCADE;
-
-DROP TABLE IF EXISTS WORK_DETAIL CASCADE;
-
-DROP TABLE IF EXISTS BILL;
-
-DROP TYPE IF EXISTS bloodtype CASCADE;
-
-DROP TYPE IF EXISTS document_type CASCADE;
-
-DROP TYPE IF EXISTS highest_education CASCADE;
-
-DROP TYPE IF EXISTS bill_status CASCADE;
-
 CREATE TYPE blood_type AS enum (
     'A',
     'B',
     'O',
     'AB'
+);
+
+CREATE TYPE active_status_type AS enum (
+    'ACTIVE',
+    'INACTIVE'
+);
+
+CREATE TYPE nationality AS enum (
+    'เมียนมา',
+    'กัมพูชา',
+    'ลาว'
 );
 
 CREATE TYPE document_type AS enum (
@@ -55,13 +40,15 @@ CREATE TYPE bill_status AS enum (
 CREATE TABLE ADDRESS (
     Id varchar(13) PRIMARY KEY,
     Address_detail_th varchar(255)  NOT NULL,
-    Address_detail_en varchar(255) NOT NULL,
     District_th varchar(255) NOT NULL,
-    District_en varchar(255) NOT NULL,
     Sub_district_th varchar(255) NOT NULL,
-    Sub_district_en varchar(255) NOT NULL,
-    Province varchar(255) NOT NULL,
-    Postel_code varchar(10) NOT NULL
+    Province_th varchar(255) NOT NULL,
+    Postal_code varchar(5) NOT NULL,
+
+    Address_detail_en varchar(255),
+    District_en varchar(255),
+    Sub_district_en varchar(255),
+    Province_en varchar(255)
 );
 
 CREATE TABLE EMPLOYER (
@@ -69,11 +56,16 @@ CREATE TABLE EMPLOYER (
     Firstname varchar(255) NOT NULL,
     Lastname varchar(255) NOT NULL,
     Business_type varchar(255) NOT NULL,
-    Status varchar(255) NOT NULL,
-    Address_id varchar(13) NOT NULL,
+    Financial_status_year int NOT NULL,
+    Financial_status_income int NOT NULL,
+    Financial_status_tax int NOT NULL,
+    Current_income int NOT NULL,
+    Income_duration int NOT NULL,
+    Status active_status_type NOT NULL,
 
     Company_name varchar(255),
 
+    Address_id varchar(13) NOT NULL,
     CONSTRAINT fk_address_id FOREIGN KEY (Address_id) REFERENCES ADDRESS (Id) ON DELETE CASCADE
 );
 
@@ -81,11 +73,11 @@ CREATE TABLE EMPLOYEE (
     Passport_number varchar(13) PRIMARY KEY,
     Firstname varchar(255) NOT NULL,
     Lastname varchar(255) NOT NULL,
-    Nationality varchar(255) NOT NULL,
+    Nationality nationality NOT NULL,
     Blood_type blood_type NOT NULL,
-    Status varchar(255) NOT NULL,
-    Address_id varchar(13) NOT NULL,
+    Status active_status_type NOT NULL,
 
+    Address_id varchar(13) NOT NULL,
     CONSTRAINT fk_address_id FOREIGN KEY (Address_id) REFERENCES ADDRESS (Id) ON DELETE CASCADE
 );
 
@@ -95,16 +87,29 @@ CREATE TABLE AGENT (
     Lastname varchar(255) NOT NULL,
     Email varchar(255) NOT NULL,
     Hashed_password varchar(255) NOT NULL,
-    Status varchar(255) NOT NULL,
-    Address_id varchar(13) NOT NULL,
+    Status active_status_type NOT NULL,
 
+    Address_id varchar(13) NOT NULL,
     CONSTRAINT fk_address_id FOREIGN KEY (Address_id) REFERENCES ADDRESS (Id) ON DELETE CASCADE
 );
 
 CREATE TABLE DOCUMENT (
     Id varchar(13) PRIMARY KEY,
     Type document_type NOT NULL,
-    Expiry_date date NOT NULL
+    Expiry_date date NOT NULL,
+
+    Employee_id varchar(13) NOT NULL,
+    CONSTRAINT fk_employee_passport_number FOREIGN KEY (Employee_id) REFERENCES Employee (Passport_number) ON DELETE CASCADE
+);
+
+CREATE TABLE EMPLOYMENT (
+    Employer_id varchar(13),
+    Employee_id varchar(13),
+    Status active_status_type NOT NULL,
+
+    PRIMARY KEY (Employer_id, Employee_id),
+    CONSTRAINT fk_employer_id FOREIGN KEY (Employer_id) REFERENCES Employer (Id) ON DELETE CASCADE,
+    CONSTRAINT fk_employee_passport_number FOREIGN KEY (Employee_id) REFERENCES Employee (Passport_number) ON DELETE CASCADE
 );
 
 CREATE TABLE EMPLOYMENT_CONTRACT (
@@ -113,10 +118,9 @@ CREATE TABLE EMPLOYMENT_CONTRACT (
     Type_of_work_en varchar(255) NOT NULL,
     Income_per_day int NOT NULL,
     Paid_income_at int NOT NULL,
-    Period_of_employment_th varchar(255) NOT NULL,
-    Period_of_employment_en varchar(255) NOT NULL,
     Working_hour_limit int NOT NULL,
     Working_day_per_week int NOT NULL,
+    Employment_period_month int NOT NULL,
     Day_off_weekly_th varchar(255) NOT NULL,
     Day_off_weekly_en varchar(255) NOT NULL,
     Day_off_holiday_th varchar(255) NOT NULL,
@@ -127,20 +131,14 @@ CREATE TABLE EMPLOYMENT_CONTRACT (
     Overtime_rate_en varchar(255) NOT NULL,
     Holiday_overtime_rate_th varchar(255) NOT NULL,
     Holiday_overtime_rate_en varchar(255) NOT NULL,
-    Employee varchar(13) NOT NULL,
-    Employer varchar(13) NOT NULL,
 
-    CONSTRAINT fk_employer_id FOREIGN KEY (Employer) REFERENCES Employer (Id) ON DELETE CASCADE,
-    CONSTRAINT fk_employee_passport_number FOREIGN KEY (Employee) REFERENCES Employee (Passport_number) ON DELETE CASCADE
+    Employer_id varchar(13) NOT NULL,
+    Employee_id varchar(13) NOT NULL,
+    CONSTRAINT fk_employment_contract_employment FOREIGN KEY (Employer_id, Employee_id) REFERENCES EMPLOYMENT (Employer_id, Employee_id) ON DELETE CASCADE
 );
 
 CREATE TABLE WP_46 (
     Id varchar(13) PRIMARY KEY,
-    Financial_status_year int NOT NULL,
-    Financial_status_income int NOT NULL,
-    Financial_status_tax int NOT NULL,
-    Current_income int NOT NULL,
-    Income_duration int NOT NULL,
     Type_of_work varchar(255) NOT NULL,
     Nature_of_work varchar(255) NOT NULL,
     Period_of_employment_year int NOT NULL,
@@ -152,9 +150,11 @@ CREATE TABLE WP_46 (
     Highes_education highest_education NOT NULL,
     Work_experience int NOT NULL,
     Reason_for_not_employing_thai_person varchar(255) NOT NULL,
-    EMP_contract_id varchar(13) NOT NULL,
+    Created_at timestamp NOT NULL,
 
-    CONSTRAINT fk_emp_contract_id FOREIGN KEY (EMP_contract_id) REFERENCES EMPLOYMENT_CONTRACT (Id) ON DELETE CASCADE
+    Employer_id varchar(13) NOT NULL,
+    Employee_id varchar(13) NOT NULL,
+    CONSTRAINT fk_employment_wp_46 FOREIGN KEY (Employer_id, Employee_id) REFERENCES EMPLOYMENT (Employer_id, Employee_id) ON DELETE CASCADE
 );
 
 CREATE TABLE WORK (
